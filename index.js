bc8c399f82a8179da4ce919ce90b76d2deecf523
JM Cléry <jmclery@gmail.com>
2018-10-22 00:50:31 +0200

    Le serveur dit 'Hello World' sur '/'

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello Node.js')
+const express = require('express')
+
+const app = express()
+
+const PORT = 9000
+const HOST = 'localhost'
+
+app.get('/', (req, res) => {
+    res.end('Bienvenue sur la home')
+})
+
+app.listen(PORT, HOST, () => console.log(`Le serveur écoute sur http://${HOST}:${PORT}`))
