14f7b3ca7bf6a38f064d27fe2c0ba4800ef6ed89
JM Cléry <jmclery@gmail.com>
2018-10-22 01:00:51 +0200

    Installe Pug

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const PORT = 9000
 const HOST = 'localhost'
 
 app.get('/', (req, res) => {
-    res.end('Bienvenue sur la home')
+    res.render('index', { name: 'JM' })
 })
 
 app.listen(PORT, HOST, () => console.log(`Le serveur écoute sur http://${HOST}:${PORT}`))
