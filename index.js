8b2eb7c8a7abdee3212519a52046af274d510e2e
JM Cléry <jmclery@gmail.com>
2018-10-22 18:48:23 +0200

    Connecte la BDD mLab au démarrage de l'app Express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const adminRouter = require('./admin.router.js')
@@ -16,4 +18,19 @@ app.use('/', blogRouter) // Traite les routes pour la partie front-office
 app.use('/admin', adminRouter) // Traite les routes pour la partie back-office (/admin)
 
 // Démarrage de l'application
-app.listen(PORT, HOST, () => console.log(`Le serveur écoute sur http://${HOST}:${PORT}`))
+// -----------------------------------------
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env // Récupère les valeurs des variables d'environnement présentes dans le fichier ".env"
+const startApp = app => { // Création d'une fonction qui renvoie une promesse résolue (ou rejetée) en fonction du résultat du "app.listen" d'Express
+    return new Promise( (resolve, reject) => {
+        const server = app.listen(PORT, HOST, resolve)
+        server.on('error', reject)
+    } );
+}
+
+mongoose
+    .connect(`mongodb://${encodeURIComponent(DB_USER)}:${encodeURIComponent(DB_PASSWORD)}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {useNewUrlParser:true})
+    .then(() => console.log('MongoDB : Connexion établie'))
+    .then(() => startApp(app))
+    .then(() => console.log(`Express : Le serveur écoute sur http://${HOST}:${PORT}`))
+    .catch(err => console.error(err.message))
